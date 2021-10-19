f9ec3cde49a72c442717405b777ea02af320e6a5
Nabiyatulmisriya <92315263+Nabiyatulmisriya@users.noreply.github.com>
2021-10-19 19:04:09 +0530

    Add files via upload

---
+++ js-task3/script.js
@@ -0,0 +1,29 @@
+let add = function (a, b) {
+  var add = a + b;
+  return console.log(add);
+};
+add(4, 5);
+
+let sub = function (a, b) {
+  var sub = a - b;
+  return console.log(sub);
+};
+sub(10, 5);
+
+let mul = function (a, b) {
+  var mul = a * b;
+  return console.log(mul);
+};
+mul(4, 5);
+
+let div = function (a, b) {
+  var div = a / b;
+  return console.log(div);
+};
+div(100, 4);
+
+let mod = function (a, b) {
+  var mod = a % b;
+  return console.log(mod);
+};
+mod(20, 3);
